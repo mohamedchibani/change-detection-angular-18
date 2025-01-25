ae894b19d1934711b18e9a69d4ae40bcb4b2c52a
Mohamed CHIBANI <mohamedchibani1996@gmail.com>
2025-01-25 17:41:26 +0100

    refactor(MessagesListComponent): implement OnInit lifecycle hook for message subscription and change detection

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,11 +15,23 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   private messagesService = inject(MessagesService);
+  private cdRef = inject(ChangeDetectorRef);
+  private destroyRef = inject(DestroyRef);
 
-  get messages() {
-    return this.messagesService.allMessages;
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    const subscription = this.messagesService.messages$.subscribe(
+      (messages) => {
+        this.messages = messages;
+        this.cdRef.markForCheck();
+      }
+    );
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   get debugOutput() {
